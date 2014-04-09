e1d6644ee796e7f0d47151360541398c3f29a704
Hannyajin <talmo.christian@gmail.com>
2014-04-09 14:56:51 +0300

    adding collisions

--- Wild.js
+++ Wild.js
@@ -14,33 +14,131 @@ var MoveEnum = {
 }
 
 var GLOBAL = {
-	SEED: 1
+	SEED: 1,
+	MAP_SIZE: 4
 }
 
 var Game = {
-	MAP_SIZE: 4,
-	board: [][],
+	SIZE: Math.round(Math.sqrt((GLOBAL.MAP_SIZE + 3) * 20)),
+	board: [this.SIZE][this.SIZE][],
+	
+	// easy simple determinstic RNG
 	random: function() {
 		var x = Math.sin(GLOBAL.SEED++) * 10000,
 		return x - Math.floor(x);
-	}
-}
+	},
 
-var Wild = {
-	MAP_SIZE: Math.round(Math.sqrt((Game.MAP_SIZE + 3) * 20)),
+
 	populate: function(species, num) {
 		while (num > 0) {
-			var row = Game.random() * Game.MAP_SIZE,
-					col = Game.random() * Game.MAP_SIZE;
-			if (Game.board[row][col] != null)
+			var row = Game.random() * this.SIZE,
+					col = Game.random() *	this.SIZE;
+			if (this.map[row][col].length > 0)
 				continue;
-			Game.board[row][col] = species;
+			this.map[row][col] = species;
 			num--;
 		}
 	},
+
+	getArea: function() {
+
+	},
+
 	iterate: function() {
-		
+		// create new board to save changes to
+		var newBoard = [this.SIZE][this.SIZE][];
+
+		// loop through current board and save changes to newBoard
+		for (var i = 0; i < SIZE; i++) {
+			for (var j = 0; j < SIZE; j++) {
+				var animal = this.board[i][j];
+				if (animal != null && animal instanceof Animal) {
+					// give animal its surroundings
+					animal.surroundings = this.getArea(i,j);
+
+					// check animals desired move
+					switch ( animal.move() ) {
+						case MoveEnum.LEFT:
+							newBoard[(i - 1 + SIZE) % SIZE][j].push(animal);
+							break;
+						case MoveEnum.RIGHT
+							newBoard[(i + 1) % SIZE][j].push(animal);
+							break;
+						case MoveEnum.DOWN:
+							newBoard[(i][(j - 1 + SIZE) % SIZE].push(animal);
+							break;
+						case MoveEnum.UP:
+							newBoard[(i][(j + 1) % SIZE].push(animal);
+							break;
+						default:
+							newBoard[i][j].push(animal);
+					}
+				}
+			}	// for loop
+		} // for loop
+
+		// update the current board to reflect the changes
+		this.board = newBoard;
+	},
+
+	// check for collisions, collisions will trigger fights
+	collide: function() {
+		for (var i = 0; i < SIZE; i++) {
+			for (var j = 0; j < SIZE; j++) {
+
+				var cell = this.board[i][j];
+				while (cell.length > 1) {
+
+					// grab two unique indexes at random
+					var r1, r2;
+					r1 = Game.random() * cell.length;
+					do {
+						r2 = Game.random() * cell.length;
+					} while (r1 === r2)
+
+					// get the corresponding animals
+					var a1 = cell[r1];
+					var a2 = cell[r2];
+
+					// THERE CAN BE ONLY ONE!
+					var atk1 = a1.fight(a2);
+					var atk2 = a2.fight(a1);
+
+					// decide the winner
+					if (atk1 === atk2) {
+							// if both chose the same attack, pick winner at random
+							cell.splice(Game.random() > 0.5 ? r1 : r2, 1);
+					} else {
+						switch (atk1) {
+							case AttackEnum.PAPER:
+								cell.splice( atk2 === AttackEnum.SCISSORS ? r1 : r2 );
+								break;
+							case AttackEnum.ROCK:
+								cell.splice( atk2 === AttackEnum.PAPER ? r1 : r2 );
+								break;
+							case AttackEnum.SCISSORS:
+								cell.splice( atk2 === AttackEnum.ROCK ? r1 : r2 );
+								break;
+							default: // no matches (error)
+								// kill the animal that gave unrecognized attack
+								cell.splice(r1, 1);
+						}
+					}
+				}
+
+			} // for loop
+		} // for loop
 	}
+
+
+}
+
+var Board = {
+	SIZE: ,
+	map: [][],
+
+	// populate / initialize the map
+	
 }
 
 var Animal = {
@@ -117,3 +215,4 @@ Wolf.move = function() {
 		return MoveEnum.LEFT;
 	return Move.DOWN;
 }
+
